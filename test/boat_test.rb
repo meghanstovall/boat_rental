210bb24e27964eac431cb22c1bdffab69e6e0682
Meghan Stovall <meghanstovall15@augustana.edu>
2020-01-01 10:34:42 -0700

    Finish iteration 1

--- test/boat_test.rb
+++ test/boat_test.rb
@@ -9,4 +9,21 @@ class BoatTest < MiniTest::Test
 
     assert_instance_of Boat, kayak
   end
+
+  def test_it_has_attributes
+    kayak = Boat.new(:kayak, 20)
+
+    assert_equal :kayak, kayak.type
+    assert_equal 20, kayak.price_per_hour
+    assert_equal 0, kayak.hours_rented
+  end
+
+  def test_can_add_hour
+    kayak = Boat.new(:kayak, 20)
+    kayak.add_hour
+    kayak.add_hour
+    kayak.add_hour
+
+    assert_equal 3, kayak.hours_rented
+  end
 end
