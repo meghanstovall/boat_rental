7e70c13c0e6c7199ac533e259ee31da2f65c2040
Meghan Stovall <meghanstovall15@augustana.edu>
2020-01-01 11:01:42 -0700

    Finish iteration 3

--- lib/dock.rb
+++ lib/dock.rb
@@ -11,4 +11,17 @@ class Dock
   def rent(boat, renter)
     @rental_log[boat] = renter
   end
+
+  def charge(boat)
+    boat_renter = @rental_log.find {|pair| pair.first == boat}
+    renter = boat_renter.last
+
+    if boat.hours_rented > @max_rental_time
+      amount = boat.price_per_hour * @max_rental_time
+    else
+      amount = boat.price_per_hour * boat.hours_rented
+    end
+
+    charge = {card_number: renter.credit_card_number, amount: amount}
+  end
 end
--- test/dock_test.rb
+++ test/dock_test.rb
@@ -35,4 +35,31 @@ class DockTest < MiniTest::Test
 
     assert_equal ({kayak_1 => patrick, kayak_2 => patrick, sup_1 => eugene}), dock.rental_log
   end
+
+  def test_can_charge_boat
+    dock = Dock.new("The Rowing Dock", 3)
+
+    kayak_1 = Boat.new(:kayak, 20)
+    kayak_2 = Boat.new(:kayak, 20)
+    sup_1 = Boat.new(:standup_paddle_board, 15)
+
+    patrick = Renter.new("Patrick Star", "4242424242424242")
+    eugene = Renter.new("Eugene Crabs", "1313131313131313")
+
+    dock.rent(kayak_1, patrick)
+    dock.rent(kayak_2, patrick)
+    dock.rent(sup_1, eugene)
+    kayak_1.add_hour
+    kayak_1.add_hour
+
+    assert_equal ({card_number: "4242424242424242", amount: 40}), dock.charge(kayak_1)
+
+    sup_1.add_hour
+    sup_1.add_hour
+    sup_1.add_hour
+    sup_1.add_hour
+    sup_1.add_hour
+
+    assert_equal ({card_number: "1313131313131313", amount: 45}), dock.charge(sup_1)
+  end
 end
